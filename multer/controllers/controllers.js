e52ab149dab5a966a2910045f9d09a3bcf0c968c
AnilHasda <hasdaanil098@gmail.com>
2024-05-01 12:08:23 +0545

    multer-updated fileFilter function in multer

--- multer/controllers/controllers.js
+++ multer/controllers/controllers.js
@@ -4,6 +4,10 @@ resp.render("index",{name:["anil","sarkar"]});
 const insertData=(req,resp)=>{
 console.log(req.body);
 console.log(req.file)
+if (req.fileFilterError) {
+    return res.status(400).send(req.fileFilterError);
+  }
+
 resp.send("data sent successful")
 }
 export {getData,insertData};
--- multer/multerConfig/multerConfig.js
+++ multer/multerConfig/multerConfig.js
@@ -8,5 +8,14 @@ filename:function (req,file,cb){
     cb(null,Date.now()+"-"+file.originalname);
 }
 });
-const upload=multer({storage}).single("profile")
+const imageFilter = function (req, file, cb) {
+    // Accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|JPG|JPEG|PNG)$/)) {
+      return cb(new Error("Only image files are allowed!"), false);
+    }
+    cb(null, true);
+  };
+  
+  
+const upload=multer({storage,fileFilter:imageFilter}).single("profile")
 export default upload;
