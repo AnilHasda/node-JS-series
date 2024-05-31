3bb922ac2ab31f83a3b8045cd7c26fa3cb911855
AnilHasda <hasdaanil098@gmail.com>
2024-05-31 15:31:35 +0545

    cloudinary file updated

--- cloudinary/Backend/Routes/uploadMedial.routes.js
+++ cloudinary/Backend/Routes/uploadMedial.routes.js
@@ -2,6 +2,6 @@ import express from "express";
 import { uploadMedia, testData } from "../Controllers/upload.controller.js";
 import { upload } from "../Middlewares/multer.middleware.js";
 let router = express.Router();
-router.route("/upload/media").post(upload.single("image"),uploadMedia);
+router.route("/upload/media").post(upload.single("image"), uploadMedia);
 router.route("/testing/route").get(testData);
 export default router;
--- cloudinary/Backend/utils/cloudinary.js
+++ cloudinary/Backend/utils/cloudinary.js
@@ -1,30 +1,32 @@
-import {v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 import fs from "fs";
 // Configuration
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_NAME, 
-    api_key: process.env.API_KEY, 
-    api_secret: process.env.API_SECRET
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
- // Upload an image
- const imageUploader=async (filePath)=>{
-    console.log(process.env.API_SECRET)
-    try{
-        if(!filePath) return "file not found";
-const uploadResult=await cloudinary.uploader.upload(filePath,{resource_type:'image'});
-if(uploadResult){
-    console.log("File uploaded successfully");
-    fs.unlinkSync(filePath);
-    return uploadResult;
-}else{
-    fs.unlinkSync(filePath);
-    console.log("Failed to upload files")
-}
-    }catch(error){
-        console.log("error occurs")
-        console.log(error);
-        return error;
+// Upload an image
+const imageUploader = async (filePath) => {
+  console.log(process.env.API_SECRET);
+  try {
+    if (!filePath) return "file not found";
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+      resource_type: "image",
+    });
+    if (uploadResult) {
+      console.log("File uploaded successfully");
+      fs.unlinkSync(filePath);
+      return uploadResult;
+    } else {
+      fs.unlinkSync(filePath);
+      console.log("Failed to upload files");
     }
- }
- export {imageUploader};
+  } catch (error) {
+    console.log("error occurs");
+    console.log(error);
+    return error;
+  }
+};
+export { imageUploader };
